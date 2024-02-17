52ee402ca37a82be42b5d8230c94375000c127f4
NAMANIND <namanrai309@gmail.com>
2024-02-17 19:48:48 +0530

    oki

--- src/sections/zoom-section.tsx
+++ src/sections/zoom-section.tsx
@@ -60,6 +60,31 @@ export default function Zoomsection() {
       src: Picture7,
       scale: scale9,
     },
+    {
+      src: Picture7,
+      scale: scale9,
+    },
+    {
+      src: Picture6,
+      scale: scale8,
+    },
+    {
+      src: Picture5,
+      scale: scale9,
+    },
+    {
+      src: Picture4,
+      scale: scale5,
+    },
+    {
+      src: Picture2,
+      scale: scale6,
+    },
+
+    {
+      src: Picture3,
+      scale: scale5,
+    },
   ];
 
   return (
